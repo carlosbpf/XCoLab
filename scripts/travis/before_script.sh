7083dd736fc430da1a432d1d1835310c965aae3a
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 15:47:40 -0200

    copying files

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,6 +2,10 @@
 # Run Mysql Create Script
 echo "Running create database script"
 mysql -u root xcolab < $TRAVIS_BUILD_DIR/scripts/travis/xcolab_Create.sql
+echo "Copying application properties file"
 cp $TRAVIS_BUILD_DIR/microservices/services/src/main/resources/application.properties $HOME/.xcolab.application.properties
-mkdir $HOME/$USER/deploy
-cat "liferay.auto.deploy.dir=$HOME/$USER/deploy" > $HOME/.xcolab.deploy.properties
+echo "Copying application properties file"
+mkdir $TRAVIS_BUILD_DIR/../deploy
+touch $HOME/.xcolab.deploy.properties
+cat "liferay.auto.deploy.dir=$TRAVIS_BUILD_DIR/../deploy" > $HOME/.xcolab.deploy.properties
+tail $HOME/.xcolab.deploy.properties
