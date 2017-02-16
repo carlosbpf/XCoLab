d089dab9ca0c1bfc2ca5b6c9ab769a0d981eca78
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 14:48:49 -0200

    added copy for app prop

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,3 +2,4 @@
 # Run Mysql Create Script
 echo "Running create database script"
 mysql -u root xcolab < $TRAVIS_BUILD_DIR/scripts/travis/xcolab_Create.sql
+cp $TRAVIS_BUILD_DIR/conf/application.properties $HOME/.xcolab.application.properties
