4d3db075eb24c8d5b9d843fcd63b2717bcf5cb85
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 15:35:25 -0200

    deployment properties set

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -3,4 +3,5 @@
 echo "Running create database script"
 mysql -u root xcolab < $TRAVIS_BUILD_DIR/scripts/travis/xcolab_Create.sql
 cp $TRAVIS_BUILD_DIR/microservices/services/src/main/resources/application.properties $HOME/.xcolab.application.properties
-
+mkdir $HOME/$USER/deploy
+cat "liferay.auto.deploy.dir=$HOME/$USER/deploy" > $HOME/.xcolab.deploy.properties
