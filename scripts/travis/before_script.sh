3c235e124e899440e8ce42ee25c970a60456e989
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 14:51:46 -0200

    before script rocks

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,4 +2,5 @@
 # Run Mysql Create Script
 echo "Running create database script"
 mysql -u root xcolab < $TRAVIS_BUILD_DIR/scripts/travis/xcolab_Create.sql
-cp $TRAVIS_BUILD_DIR/conf/application.properties $HOME/.xcolab.application.properties
+cp $TRAVIS_BUILD_DIR/microservices/services/src/main/resources/application.properties $HOME/.xcolab.application.properties
+
