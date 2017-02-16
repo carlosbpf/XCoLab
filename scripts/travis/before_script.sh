e3f5f6e2f33c2729ef030b11b4cbe4008644074d
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 12:06:59 -0200

    prints to build

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # Run Mysql Create Script
+echo "Running create database script"
 mysql -u root xcolab < $TRAVIS_BUILD_DIR/scripts/travis/xcolab_Create.sql
 
 #Roma client install
-cd $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR/..
+echo "Before git clone ROMA repo"
 git clone https://github.com/CCI-MIT/ROMA.git
-cd $TRAVIS_BUILD_DIR/ROMA/core/
+echo "After git clone ROMA repo"
+cd $TRAVIS_BUILD_DIR/../ROMA/core/
+echo "Before ROMA core mvn"
 mvn compile package install -DskipTests=true
-cd $TRAVIS_BUILD_DIR/client/
+cd $TRAVIS_BUILD_DIR/../client/
+echo "After ROMA core mvn"
+echo "Before ROMA client mvn"
 mvn compile package install -DskipTests=true
-cd ../
+echo "Before ROMA client mvn"
+cd $TRAVIS_BUILD_DIR
 rm -rf ROMA/
 cd $TRAVIS_BUILD_DIR
