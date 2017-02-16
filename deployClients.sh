c4bda6a4c95a6c5e28046e8a0a455f09cd0761e6
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 20:50:45 -0200

    new deploy no install

--- deployClients.sh
+++ deployClients.sh
@@ -2,42 +2,56 @@
 
 # stop script if any build fails
 set -e
-
-mvn install -N
+echo "#####################################################################################"
+echo "Deploying parent dependencies"
+echo "#####################################################################################"
+LOG="-q -B"
+mvn install -N $LOG
 cd microservices/
-mvn install -N
+mvn install -N $LOG
 cd util/
-mvn install -N
+mvn install -N $LOG
 cd ../clients/
-mvn install -N
+mvn install -N $LOG
+echo "#####################################################################################"
+echo "Deploying xcolab-utils"
+echo "#####################################################################################"
 cd ../util/xcolab-utils/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG 
+echo "#####################################################################################"
+echo "Deploying entity-utils"
+echo "#####################################################################################"
 cd ../../../
 cd microservices/clients/emails-client
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../members-client
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../admin-client/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../comment-client/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../activities-client/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../modeling-client/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../contestproposal-client/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../../util/entity-utils/
-mvn clean compile package install clean
+mvn clean compile package install clean $LOG
 cd ../../..
 
+echo "#####################################################################################"
+echo "Deploying clients"
+echo " "
 cd microservices/clients
 for D in *-client; do
     if [ -d "${D}" ]; then
         cd $D
+        echo " - $D"
 		pwd
-		mvn clean compile package install clean
+		mvn clean compile package install clean $LOG
 		cd ..
     fi
 done
+echo "#####################################################################################"
 cd ../..
