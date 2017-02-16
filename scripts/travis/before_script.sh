ef6160204af5c48f60ea76974673d53c1d643a61
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 11:26:16 -0200

    Roma dependencies fixed?

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -1,3 +1,9 @@
 # Run Mysql Create Script
 mysql -u travis -p xcolab < xcolab_Create.sql
 
+#Roma client install
+git clone https://github.com/CCI-MIT/ROMA.git
+cd ROMA/core/
+mvn compile package install -DskipTests=true
+cd ../client/
+mvn compile package install -DskipTests=true
