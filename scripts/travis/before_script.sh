8734c4495a72b302e124634865b3d90c68499e7f
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 11:45:51 -0200

    fixing broken paths

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,11 +2,12 @@
 mysql -u travis -p xcolab < xcolab_Create.sql
 
 #Roma client install
-cd ../
+cd $TRAVIS_BUILD_DIR
 git clone https://github.com/CCI-MIT/ROMA.git
-cd ROMA/core/
+cd $TRAVIS_BUILD_DIR/ROMA/core/
 mvn compile package install -DskipTests=true
-cd ../client/
+cd $TRAVIS_BUILD_DIR/client/
 mvn compile package install -DskipTests=true
 cd ../
 rm -rf ROMA/
+cd $TRAVIS_BUILD_DIR
