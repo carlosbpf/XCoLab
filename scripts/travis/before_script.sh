eec2c252f4c41a82049fecafb42f7edd14a96d36
Carlos Botelho <carlosbpf@gmail.com>
2017-02-16 11:35:55 -0200

    new attempt using before script

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -2,8 +2,11 @@
 mysql -u travis -p xcolab < xcolab_Create.sql
 
 #Roma client install
+cd ../
 git clone https://github.com/CCI-MIT/ROMA.git
 cd ROMA/core/
 mvn compile package install -DskipTests=true
 cd ../client/
 mvn compile package install -DskipTests=true
+cd ../
+rm -rf ROMA/
